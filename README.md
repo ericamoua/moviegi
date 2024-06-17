24ca3d4e5d265137d7d98a98757e4a54c3ba5ede
EricaMoua <erica.moua568@gmail.com>
2024-06-16 22:51:51 -0400

    read me

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # moviegi
 node.js
+1. make sure you npm install the packages
+2. then run node server.js in the terminal 
+3. go live on html 
--- frontend/front.js
+++
@@ -1,7 +0,0 @@
-import { searchMovie } from "../movie";
-
-// create a function that gets html 
-document.getElementById('buttonInput').addEventListener("click", function (e) {
-    const moiveName = document.getElementById("searchInput").value;
-    searchMovie(moiveName);
-  });
